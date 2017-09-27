38ff7c86cc4cdf44965427124a3c8e37e3182d51
Hoegie <mezenhof8@gmail.com>
2017-09-27 14:10:34 +0200

    3rd commit

---
+++ pokerhook.sh
@@ -0,0 +1,4 @@
+date >> /app/nodeprojects/github/logs/pokerhook.log 2>&1
+cd /app/nodeprojects/github/poker
+git pull https://github.com/Hoegie/poker.git >> /app/nodeprojects/github/logs/pokerhook.log 2>&1
+
